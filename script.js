ac33b09b2979863de24c26cbad98d2663909874f
H-akim <92632018+H-akim@users.noreply.github.com>
2023-03-04 15:19:40 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+let textarea = document.getElementById("text");
+let result = document.getElementById("result");
+let select = document.getElementById("select");
+
+textarea.addEventListener("input", ()=>{
+    make_result();
+});
+
+select.addEventListener("change", ()=>{
+    make_result();
+});
+
+function make_result() {
+    if (select.value == "decryption") {
+        result.value = window.atob(textarea.value) ;
+    }else{
+        result.value = window.btoa(textarea.value) ;
+    }
+}
